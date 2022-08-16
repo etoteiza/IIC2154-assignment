f60a811b685e1bfe3b1a1e6cbd8f799b4d0bb73a
etoteiza <etoteiza@uc.cl>
2022-08-16 16:20:28 -0400

    top 10 users

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from functions.load_data import load_data
 from functions.top_retweets import top_retweets
+from functions.top_users_tweets import top_users_tweets
 
 if __name__ == "__main__":
     
     data = load_data()
 
     top_retweets(data)
+
+    top_users_tweets(data)
